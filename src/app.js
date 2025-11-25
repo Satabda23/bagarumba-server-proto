79e3eefded623313dc66f5f067d76056ffd0131c
ansen01-svg <ansenbeyc73@gmail.com>
2025-11-25 21:04:04 +0530

    added frontend url

--- src/app.js
+++ src/app.js
@@ -14,7 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
